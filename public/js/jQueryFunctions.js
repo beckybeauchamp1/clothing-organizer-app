11fbe4fdc3dbd0589ea9498a2195fdb5792ec6e0
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-06-19 09:47:16 -0700

    Adds img figure caption plugin

--- public/js/jQueryFunctions.js
+++ public/js/jQueryFunctions.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $(".main-menu").on("click", changeBackgroundHome);
 
   function changeBackgroundHome(){
-    $("body").css("background", "linear-gradient( rgba(255, 255, 255, 0.6),rgba(0, 0, 0, 0.2)),url('./CSS/plan2.jpg')");
+    $("body").css("background", "linear-gradient( rgba(255, 255, 255, 0.6),rgba(0, 0, 0, 0.2)),url('./images/plan2.jpg')");
     $("body").css("background-size", "cover");
     $("body").css("background-attachment", "fixed");
   };
--- public/js/outfits/outfitController.js
+++ public/js/outfits/outfitController.js
@@ -13,14 +13,14 @@ angular.module('outfits')
   };
 
   $scope.changeBackground = function(){
-    $("body").css("background", "linear-gradient( rgba(255, 255, 255, 0.8),rgba(0, 0, 0, 0.2)),url('./CSS/wood.jpg')");
+    $("body").css("background", "linear-gradient( rgba(255, 255, 255, 0.9),rgba(255, 255, 255, 0.6)),url('./images/wood.jpg')");
   }
 
   $scope.changeBackground();
 
   $scope.changeBackgroundHome = function(){
     console.log("working")
-    $("body").css("background-image", "url('./CSS/plan2.jpg')");
+    $("body").css("background-image", "url('./images/plan2.jpg')");
   };
 
 
